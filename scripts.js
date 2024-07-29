16546172d4dd9fbfb1988fa3c51ecc172e9475c2
aristea23 <aristeajuniper@gmail.com>
2024-07-29 19:07:18 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,3 +21,9 @@ var closeBtn = document.querySelector(".close");
 closeBtn.addEventListener("click", function() {
     modal.style.display = "none";
 });
+
+window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
