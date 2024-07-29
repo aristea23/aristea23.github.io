b562369aba743a28a03011b25ecfdc6fa7050907
aristea23 <aristeajuniper@gmail.com>
2024-07-29 18:57:35 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById("modal");
+
+var modalImg = document.getElementById("modal-image");
+
+var captionText = document.getElementById("caption");
+
+var images = document.querySelectorAll(".image-grid-item");
+
+images.forEach(function(image) {
+    image.addEventListener("click", function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    });
+});
+
+
+var closeBtn = document.querySelector(".close");
+
+
+closeBtn.addEventListener("click", function() {
+    modal.style.display = "none";
+});
